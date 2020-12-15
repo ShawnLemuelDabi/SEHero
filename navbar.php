4a9bd2bba29670e31576aeb1df54ca58d81c7c6c
ShawnDabi <2001401@sit.singaporetech.edu.sg>
2020-12-15 14:52:52 +0800

    Isolated CDNs and PHP-fied the template

--- navbar.php
+++
@@ -1,8 +0,0 @@
-<!-- Navigation -->
-<nav class="navbar navbar-light bg-light fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="howToUseMe.php">How to use me</a>
-        <a class="navbar-brand" href="index.php#year1">Year 1</a>
-        <a class="navbar-brand" href="index.php#year2">Year 2</a>
-    </div>
-</nav>
